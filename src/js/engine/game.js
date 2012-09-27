5f5760b59bea9cbe28ea43e3094f83b4020792aa
Gabor Heja <gheja@bagheera>
2012-09-28 00:21:16 +0200

    Reverted changes of f37b9439e256c84e0523d248f0711c3e3852393a

--- src/js/engine/game.js
+++ src/js/engine/game.js
@@ -92,7 +92,7 @@ gGameObject.prototype.UpdateDynamicValues = function(objects)
 		
 		// check if the ... side of "this" hits "obj"
 		// top
-		if (this.pos_y >= obj.pos_y + obj.height && this.pos_y + this.speed_y <= obj.pos_y + obj.height &&
+		if (this.pos_y >= obj.pos_y + obj.height && this.pos_y + this.speed_y < obj.pos_y + obj.height &&
 			this.pos_x < obj.pos_x + obj.width && this.pos_x + this.width > obj.pos_x)
 		{
 			obj.onCollide(this, 0);
@@ -100,7 +100,7 @@ gGameObject.prototype.UpdateDynamicValues = function(objects)
 		}
 		
 		// bottom
-		if (this.pos_y + this.height <= obj.pos_y && this.pos_y + this.height + this.speed_y >= obj.pos_y &&
+		if (this.pos_y + this.height <= obj.pos_y && this.pos_y + this.height + this.speed_y > obj.pos_y &&
 			this.pos_x < obj.pos_x + obj.width && this.pos_x + this.width > obj.pos_x)
 		{
 			obj.onCollide(this, 2);
